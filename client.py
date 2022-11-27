f006780f248042b0e0f49d51980610597ec679e8
Jak Cullinane <jak.cullinane@mycit.ie>
2022-11-27 19:54:52 +0000

    initial commit

---
+++ client.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 19:48:15 2022
+
+@author: culli
+"""
+
+import socket
+
+SERVER = "127.0.0.1"
+PORT = 64002
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((SERVER, PORT))
+
+response = 'Y'
+data_to_send = "Hello!!!!"
+
+while response == 'Y':
+    sock.sendall(bytes(data_to_send, 'UTF-8'))
+    data = sock.recv(1024)
+    print('Received', repr(data))
+    response = input("Send again (Y/N)? ")
+    if response == 'Y':
+        data_to_send = input("Enter data: ")
+
+sock.close()
